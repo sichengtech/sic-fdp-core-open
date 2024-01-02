b7126f9a490187dfaf6fe5f1055478ead08a3405
同步专用 <elf8848@aliyun.com>
2024-01-02 17:30:12 +0800

    合并代码 2024-01-02 17:30:12

--- src/main/java/com/sicheng/common/persistence/dialect/db/DB2Dialect.java
+++ src/main/java/com/sicheng/common/persistence/dialect/db/DB2Dialect.java
@@ -17,7 +17,7 @@ import com.sicheng.common.persistence.dialect.Dialect;
 /**
  * DB2的分页数据库方言实现
  *
- * @author poplar.yfyang
+ * @author admin
  * @version 1.0 2010-10-10 下午12:31
  * @since JDK 1.5
  */
